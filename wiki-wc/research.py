4d877d7d9562bb17babe314472a764fcd3608e9f
Cristiano Alves da Silva Júnir <ishiikurisu@yahoo.com>
2018-01-24 17:27:09 -0200

    Refactoring code

--- wiki-wc/research.py
+++ wiki-wc/research.py
@@ -2,25 +2,30 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 class Researcher:
-    def __init__(self, qty):
-        self.limit = qty
+    def __init__(self, limit):
+        """Starts the main variables that will be used in this process"""
+        self.limit = limit
         self.debug = False
         self.pages = []
         self.how_many = 0
 
     def study_from(self, start_point):
+        """Starts the main study loop from a starting wikipedia page"""
         self.pages.append(start_point)
         self.how_many = 1
 
         while len(self.pages) > 0:
             page = self.pages[0]
             del self.pages[0]
+            # IDEA Try to turn this turn this into a parallel process
             self.study(page)
-            
+
         if self.debug:
             print('...')
 
     def study(self, page):
+        """Study a page by parsing its HTML, adding the next pages to be followed
+        and analyzing the current page text."""
         html = urllib.request.urlopen(page).read()
         soup = BeautifulSoup(html, 'html.parser')
         div = soup.find('div', {'id': 'mw-content-text'})
@@ -29,8 +34,8 @@ class Researcher:
         for link in links:
             try:
                 next_page = self.get_link(link)
-                if self.is_valid(next_page):
-                    self.work_with(next_page)
+                if next_page is not None:
+                    self.try_to_add(next_page)
             except UnicodeEncodeError:
                 pass
 
@@ -51,15 +56,14 @@ class Researcher:
             except UnicodeEncodeError:
                 pass
 
-    def work_with(self, page):
+    def try_to_add(self, page):
+        """Tries to add another page to the study process."""
         if self.how_many < self.limit:
             self.pages.append(page)
             self.how_many += 1
 
-    def is_valid(self, page):
-        return page is not None
-
     def get_link(self, a):
+        """Gets a link from a Beautiful Soup anchor element."""
         outlet = None
         try:
             href = a['href']
@@ -70,9 +74,10 @@ class Researcher:
         return outlet
 
     def get_text(self, p):
-        text = ' '.join(text.strip() for text in p.find_all(text=True, recursive=True))
-        return text
+        """Gets the text from a beautiful soup paragraph element."""
+        return ' '.join(text.strip() for text in p.find_all(text=True, recursive=True))
 
     def analyze(self, text):
+        """Studies the content of the text as proposed in this study."""
         # IDEA Create an analyzer class that will count words per document and give an opinion.
         pass
