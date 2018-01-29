e32e0b85569dd0ca2c43850270083438f803d28d
Cristiano Alves da Silva Júnir <ishiikurisu@yahoo.com>
2018-01-29 14:18:00 -0200

    Laying out basic steps in the Tulip integration with Python shell project

--- training/indie-tulip/main.py
+++ training/indie-tulip/main.py
@@ -1,3 +1,7 @@
+from tulip import tlp
+
 if __name__ == '__main__':
 	print('# Indie Tulip')
-	# TODO Complete me!
+	# TODO Load file and turn it into a Tulip graph
+	# TODO Save file as a tulip file
+	# TODO Analyze graph and genererate a plot with Tulip
