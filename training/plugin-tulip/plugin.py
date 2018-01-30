326c3785b576e9219966bdfaffbcb997e778785f
Cristiano Alves da Silva Júnir <ishiikurisu@yahoo.com>
2018-01-30 15:24:06 -0200

    Learnt how to walk around a Tulip graph

--- training/plugin-tulip/plugin.py
+++ training/plugin-tulip/plugin.py
@@ -15,11 +15,20 @@ class EigencentralityAlgorithm(tlp.DoubleAlgorithm):
     def run(self):
         result = self.dataSet['result']        
         lambdaProperty = float(self.dataSet['lambda'])
+        centralities = self.calculateCentrality(self.graph, lambdaProperty)
 
         for node in self.graph.getNodes():
-            result[node] = lambdaProperty
-
+            result[node] = centralities[node]
+        
         return True
+
+    def calculateCentrality(self, V, l):
+        """Calculates the eigenvector centrality on the Tulip graph `V` based
+        on the required lambda constant, given by `l`."""
+        # TODO Calculate the real eigenvector centrality for each node
+        ds = { v: V.deg(v) for v in V.getNodes() }
+        ec = { v: sum(map(lambda w: ds[w], V.getInOutNodes(v))) for v in V.getNodes() }
+        return ec
 
 # The line below does the magic to register the plugin to the plugin database
 # and updates the GUI to make it accessible through the menus.
@@ -29,5 +38,5 @@ tulipplugins.registerPluginOfGroup(
     "Cristiano Silva Jr. <cristianoalvesjr@gmail.com>", 
     "30/01/2018", 
     "No info yet.", 
-    "0.1", 
+    "0.2", 
     "Python")
