4f67c401a68054a41b6680c4b52fdd7dce81469d
Cristiano Alves da Silva Júnior <ishiikurisu@yahoo.com>
2018-02-09 16:13:18 -0200

    Generating and export D*D matrix

--- wiki-wc/calculator.py
+++ wiki-wc/calculator.py
@@ -21,13 +21,28 @@ def build_term_frequency_matrix(docs):
     return terms, matrix
 
 def calculate_tfidf(tf):
-    """Calculates a TF.IDF matrix from a Term * Document matrix."""
+    """Calculates a TF.IDF matrix from a Document * Term matrix."""
     N, T = tf.shape
     tfidf = numpy.zeros_like(tf)
 
     for t in range(T):
-        df = sum(tf[:, t])
+        df = sum(map(lambda x: 1 if x > 0 else 0, tf[:, t]))
         for d in range(N):
             tfidf[d, t] = tf[d, t] * numpy.log(N/df)
 
     return tfidf
+
+def calculate_document_similarity(tfidf):
+    """Calculate the similarity between all documents in a TF.IDF D*T matrix."""
+    return numpy.dot(tfidf, tfidf.T)
+
+def calculate_term_similarity(tfidf):
+    """Calculate the similarity between all terms in a TF.IDF D*T matrix."""
+    return calculate_document_similarity(tfidf.T)
+
+def export(matrix, filename):
+    M, _ = matrix.shape
+    with open(filename, 'w') as fp:
+        for m in range(M):
+            line = ';'.join(map(lambda x: '%.5f' % (x), matrix[m, :]))
+            fp.write(line + '\n')
--- wiki-wc/research.py
+++ wiki-wc/research.py
@@ -86,12 +86,13 @@ class Researcher:
         """Analyzes all the documents after the download is completed."""
         terms, td_matrix = calculator.build_term_frequency_matrix(self.docs)
         tfidf = calculator.calculate_tfidf(td_matrix)
+        dd_matrix = calculator.calculate_document_similarity(tfidf)
+        calculator.export(dd_matrix, 'DD.csv')
+
         if self.debug:
             print('--- # Analysis')
             print('how many: %d' % (self.how_many))
             print('terms: >')
             print(terms)
-            print('tfidf matrix: >')
-            print(tfidf)
-        # TODO Calculate D*D matrix
-        # TODO Export D*D matrix to CSV
+            print('D*D matrix: >')
+            print(dd_matrix)
