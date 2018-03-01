1b0cdd0ab5c5f40127012d90180fec480ac1675d
Cristiano Alves da Silva Júnior <ishiikurisu@yahoo.com>
2018-03-01 09:52:46 -0300

    Displaying historical evolution of acceptances

--- alluvial/collect.py
+++ alluvial/collect.py
@@ -4,7 +4,7 @@ import flow
 
 def get_all_valid_ids(config):
     """Stores in memory a list with ids based on the configuration."""
-    ids = set()
+    ids = {}
     years = config['years']
     src = config['working']
     valid_program = config['program']
@@ -13,16 +13,24 @@ def get_all_valid_ids(config):
         print('Getting ids from {0}'.format(year))
         excelname = config['years'][year]
         current_ids = excel.get_ids_from_program(src + excelname, valid_program)
-        # IDEA List these ids by year when saving
-        ids |= current_ids
+        for current_id in current_ids:
+            if current_id not in ids:
+                ids[current_id] = set()
+            ids[current_id].add(year)
 
     return ids
 
 def save_all_valid_ids(config, valid_ids):
     csvname = config['working'] + 'valid.csv'
+    years = config['years']
+
     with open(csvname, 'w') as fp:
+        fp.write('id;' + ';'.join(years) + '\n')
         for valid_id in valid_ids:
-            fp.write('{0};\n'.format(valid_id))
+            p = valid_ids[valid_id] # of participation
+            fp.write('{0};{1}\n'.format(valid_id,
+                                        ';'.join(map(lambda y: '1' if y in p else ' ',
+                                                     years))))
 
 if __name__ == '__main__':
     config = flow.load_config(sys.argv[1])
