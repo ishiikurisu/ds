c62062e8d48e0687aa0a54c83273987806d0f665
Cristiano Alves da Silva Júnior <ishiikurisu@yahoo.com>
2018-03-05 10:48:55 -0300

    Making some adjustments

--- alluvial/flow3.py
+++ alluvial/flow3.py
@@ -1,36 +1,8 @@
 import sys
 import json
 import excel
+import flow
 
-def load_config(where):
-    """Loads the configuration file."""
-    config = None
-    with open(where, 'r') as fp:
-        config = json.loads(fp.read())
-    return config
-
-def get_all_ids(config):
-    """Stores in memory a list with ids based on the configuration."""
-    ids = set()
-    years = config['years']
-    src = config['working']
-
-    print('# Getting all ids')
-    for year in years:
-        print('Getting ids from {0}'.format(year))
-        excelname = config['years'][year]
-        current_ids = excel.get_ids(src + excelname)
-        for current_id in current_ids:
-            ids.add(current_id)
-
-    return ids
-
-def save_all_ids(ids, config):
-    csvname = config['working'] + 'ids.csv'
-    content = ''.join(map(lambda s: '{0}\n'.format(s), ids))
-    with open(csvname, 'w') as fp:
-        fp.write(content)
-    return csvname
 
 def extract_groups(config, operation=excel.group_coordinations_by_id):
     groups = {}
@@ -82,6 +54,6 @@ def consolidate_groups(config, ids, groups):
 
 
 if __name__ == '__main__':
-    config = load_config(sys.argv[1])
+    config = flow.load_config(sys.argv[1])
     ids, groups = extract_groups(config)
     consolidate_groups(config, ids, groups)
