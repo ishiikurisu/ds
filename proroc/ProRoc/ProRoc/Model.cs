f68836bd41898c077be48a84e1425dbf4b3349a1
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-03-28 10:56:25 -0300

    Implementing ROC weights

---
+++ proroc/ProRoc/ProRoc/Model.cs
@@ -0,0 +1,40 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProRoc
+{
+    public static class Model
+    {
+        /// <summary>
+        /// Turns a list of numbers into ROC weights. WARNING! Doesn't check the input 
+        /// consistency yet.
+        /// </summary>
+        /// <param name="ranking">The original weights ranking. Must be a list of integers 
+        /// containing all integers from 1 to n, where n is the length of the list.</param>
+        /// <returns>The converted ROC weights</returns>
+        [ExcelFunction(Description = "Turns a list of numbers into ROC weights.")]
+        public static double[] Roc(double[] ranking)
+        {
+            int n = ranking.Length;
+            double[] w = new double[n];
+
+            // IDEA Check input consistency, that is, if it contains only numbers from 1 to n.
+            for (int i = 0; i < n; ++i)
+            {
+                double j = ranking[i];
+                double wj = 0;
+                for (double k = j; k <= n; ++k)
+                {
+                    wj += 1 / k;
+                }
+                w[i] = wj/n;
+            }
+
+            return w;
+        }
+    }
+}
--- proroc/ProRoc/ProRocTest/ProRocTest.cs
+++ proroc/ProRoc/ProRocTest/ProRocTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using ProRoc;
+using System;
 
 namespace ProRocTest
 {
@@ -12,6 +13,31 @@ namespace ProRocTest
         {
             string output = ExampleStuff.SayHi("Joe");
             Assert.AreEqual("Hi Joe", output);
+        }
+
+        [Test]
+        public void TestRocWeightConvertionWorks()
+        {
+            double[] input = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            double[] answer = Model.Roc(input);
+            double[] correct = new double[]
+            {
+                0.29289,
+                0.19289,
+                0.14289,
+                0.10956,
+                0.08456,
+                0.06456,
+                0.04789,
+                0.03361,
+                0.02111,
+                0.01000
+            };
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Assert.True((answer[i] >= correct[i]*0.99) && (answer[i] <= correct[i] * 1.01));
+            }
         }
     }
 }
