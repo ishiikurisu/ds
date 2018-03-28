6f23a5fb23b269d37a93c8968ce52f65c890e0db
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-03-28 09:40:57 -0300

    Setup of ProRoc C# project

---
+++ proroc/ProRoc/ProRoc/ProRocTest/ProRocTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ProRoc;
+
+namespace ProRocTest
+{
+    [TestFixture]
+    public class ProRocTest
+    {
+
+        [Test]
+        public void TestSayHiFunction()
+        {
+            string output = ExampleStuff.SayHi("Joe");
+            Assert.AreEqual("Hi Joe", output);
+        }
+    }
+}
