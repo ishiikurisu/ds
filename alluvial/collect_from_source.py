09406f5da3efa2eb300af7864f07ee93001348c6
Cristiano Alves da Silva Júnior <ishiikurisu@yahoo.com>
2018-04-10 10:27:21 -0300

    First parsing of the new source table

--- alluvial/collect_from_source.py
+++ alluvial/collect_from_source.py
@@ -1,102 +1,86 @@
+# -*- coding: utf-8 -*-
 import sys
 import util
 
-def get_years_from_line(line, fields):
-    """
-    Gets the yearly valid period from the given line. Throws a `ValueError` exception if the
-    desired fields do not contain valid data.
-    """
-    stuff = line.strip().split('\t')
-    process_id = stuff[fields.index('Código Processo Formatado')]
-    start = int(stuff[fields.index('Data Início Contrato')].split(' ')[0].split('/')[-1])
-    ending = int(stuff[fields.index('Data Término Contrato')].split(' ')[0].split('/')[-1])
-    start = int(process_id.split('/')[1].split('-')[0]) if start < 1970 else start
-    ending += 1 if start == ending else 0
-    return start, ending
+def parse_line(stuff, fields, data={}):
+    year = int(stuff[fields.index('Data Ano Pagamento')])
+    cpf = util.fix_id(stuff[fields.index('CPF')])
+    coordination = stuff[fields.index('Código Comitê Assessor')]
 
+    # Fact checking
+    if cpf == '0'*11:
+        raise ValueError
 
-def treat_line(line, fields, data={}):
-    """
-    Updates the information in the box based on the current line. Returns the input hashmap with
-    incremented information.
-    """
-    try:
-        stuff = line.strip().split('\t')
-        current_id = stuff[fields.index('CPF')]
-        coordination = stuff[fields.index('Código Comitê Assessor')]
-        start, ending = get_years_from_line(line, fields)
-    except ValueError:
-        return data
+    # Parsing data
+    if cpf not in data:
+        data[cpf] = {}
+    if year not in data[cpf]:
+        data[cpf][year] = ''
+    # TODO Improve this coordination stacking
+    data[cpf][year] += coordination
 
-    if len(current_id) == 11:
-        if current_id not in data:
-            data[current_id] = {}
-            data[current_id + '*'] = {}
-        for year in range(start, ending):
-            current = data[current_id].get(year)
-            if current is None:
-                data[current_id][year] = coordination
-            else:
-                data[current_id + '*'][year] = coordination
-
-    return data
+    return year, data
 
 def get_stuff(config):
     """
-    Loads the revelant data from the source data set. Returns a hashmap relating each id to another
-    dictionary, relating each year to a coordination.
+    Reads the fish source table to get the desired data. Returns:
+    1. a `list` with all years in the process.
+    2. a `dict` where each key is an ID and each value is another `dict`, where
+    each key is a year and each value is the coordination that ID belonged to
+    that year.
     """
-    years = set()
+    min_year = 3000
+    max_year = 1000
     data = {}
-    src = config['working'] + config['source']
 
-    with open(src, 'r') as fp:
+    with open(config['working'] + config['source'], 'r', encoding='utf-8') as fp:
         first_line = True
-        fields = []
 
-        for line in fp.readlines()[2:]:
+        for line in fp:
+            stuff = line.strip().split('\t')
             if first_line:
-                fields = line.strip().split('\t')
+                fields = stuff
                 first_line = False
             else:
-                line = '0' + line if line[0] == '\t' else line
-                data = treat_line(line, fields, data)
                 try:
-                    start, ending = get_years_from_line(line, fields)
-                    for year in range(start, ending):
-                        years.add(year)
-                except:
-                    pass
+                    year, data = parse_line(stuff, fields, data)
+                    if year > max_year:
+                        max_year = year
+                    elif year < min_year:
+                        min_year = year
+                except ValueError:
+                    print('discarded entry!')
 
-    years = list(sorted(years))
+    years = list(range(min_year, max_year+1))
+
     return years, data
 
 def get_output(config):
-    """Generates an output file name from the source for this procedure."""
-    return config['working'] + 'source.csv'
+    """
+    Creates an output file name for the current procedure.
+    """
+    inlet = config['working'] + config['source']
+    return '.'.join(inlet.split('.')[0:-1]) + '_processed.csv'
 
 def save_stuff(config, years, data):
     """
-    Saves relevant data to a "source.csv" file in the working folder set in the configuration
-    structure.
+    Stores data in memory
     """
-    with open(config['working'] + 'source.csv', 'w') as fp:
-        first_line = 'ids\t' + '\t'.join(map(str, years)) + '\n'
-        fp.write(first_line)
-        for current_id in data.keys():
+    with open(get_output(config), 'w') as fp:
+        fp.write('CPF\t{0}\n'.format('\t'.join(map(str, years))))
+        for cpf in data:
+            states = []
             appeared = False
-            content = ["'{0}'".format(current_id)]
-            coordinations = data[current_id]
+            coordinations = data[cpf]
             for year in years:
-                current_state = 'never'
+                state = 'never'
                 if year in coordinations:
-                    current_state = coordinations[year]
+                    state = coordinations[year]
                     appeared = True
                 elif appeared:
-                    current_state = 'nope'
-                content.append(current_state)
-            line = '\t'.join(content) + '\n'
-            fp.write(line)
+                    state = 'nope'
+                states.append(state)
+            fp.write("'{0}'\t{1}\n".format(cpf, '\t'.join(states)))
 
 if __name__ == '__main__':
     config = util.load_config(sys.argv[1])
