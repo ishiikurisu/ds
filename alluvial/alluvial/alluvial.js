2b9a1a36b0b7d101d9e8f5bcdb431ce7caec8934
Cristiano Alves da Silva Júnior <cristianoalvesjr@gmail.com>
2018-05-25 09:44:39 -0300

    Loading alluvial table

--- alluvial/alluvial/alluvial.js
+++ alluvial/alluvial/alluvial.js
@@ -1,3 +1,18 @@
-// MAIN PROCEDURE
+const fs = require("fs");
 
-console.log("Hello world!");
+function parseTsv(raw) {
+	lines = raw.split("\n");
+	return lines.slice(1, lines.length).map(line => {
+		var fields = line.split("\t");
+		return fields.slice(2, fields.length);
+	});
+}
+
+var sourceFile = process.argv[2];
+fs.readFile(sourceFile, 'utf8', (err, contents) => {
+	if (err) throw err;
+	var table = parseTsv(contents);
+	// TODO Create list of bars in each year from table
+	// TODO Create list of transistions between years from table
+	// TODO Draw graph as an alluvial diagram
+});
