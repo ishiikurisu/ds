0c03e357a26f62e62bd29ea7718ffcc56e76bbc4
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2018-07-13 10:19:16 -0300

    Stuyding book chapters

--- universal/analyze.py
+++ universal/analyze.py
@@ -5,12 +5,6 @@ import os
 import os.path
 import xml.etree.ElementTree
 
-def get_all_cv(config):
-    p = config['pwd'] + config['cv']
-    all = [f for f in os.listdir(p) if os.path.isfile(os.path.join(p, f))]
-    all_cv = [p+f for f in all if '.xml' in f]
-    return all_cv
-
 def get_fields_from_phd(cv):
     outlet = []
 
@@ -40,21 +34,53 @@ def get_fields_from_phd(cv):
         outlet = None
     return outlet
 
+def get_fields_from_book_chapters(cv):
+    outlet = []
+    root = xml.etree.ElementTree.parse(cv).getroot()
+
+    producao_bibliografica = root.find('PRODUCAO-BIBLIOGRAFICA')
+    if producao_bibliografica is not None:
+        livros_e_capitulos = producao_bibliografica.find('LIVROS-E-CAPITULOS')
+        if livros_e_capitulos is not None:
+            coisas_publicadas = livros_e_capitulos.find('CAPITULOS-DE-LIVROS-PUBLICADOS')
+            if coisas_publicadas is not None:
+                todos_capitulos = coisas_publicadas.findall('CAPITULO-DE-LIVRO-PUBLICADO')
+                for capitulo in todos_capitulos:
+                    areas_do_conhecimento = capitulo.find('AREAS-DO-CONHECIMENTO')
+                    if areas_do_conhecimento is not None:
+                        areas_do_conhecimento = areas_do_conhecimento.getchildren()
+                        for area_do_conhecimento in areas_do_conhecimento:
+                            area = area_do_conhecimento.attrib.get('NOME-GRANDE-AREA-DO-CONHECIMENTO')
+                            if area is not None:
+                                outlet.append(area)
+                            area = area_do_conhecimento.attrib.get('NOME-AREA-DO-CONHECIMENTO')
+                            if area is not None:
+                                outlet.append(area)
+
+
+    return None if len(outlet) == 0 else outlet
 
 if __name__ == '__main__':
     config = util.load_config(sys.argv[1])
-    all_cv = get_all_cv(config)
+
+    # Getting all CV
+    p = config['pwd'] + config['cv']
+    all = [f for f in os.listdir(p) if os.path.isfile(os.path.join(p, f))]
+    all_cv = [p+f for f in all if '.xml' in f]
+
+    # Analyzing CV
     stuff = {}
     invalid = []
     for cv in all_cv:
-        fields = get_fields_from_phd(cv)
+        fields = get_fields_from_book_chapters(cv)
         if fields is not None:
             stuff[cv] = fields
         else:
             invalid.append(cv)
 
+    # Printing relevant information from data
     print(stuff)
     for cv in invalid:
-        print(cv + 'is invalid')
+        print(cv + ' is invalid')
     print('invalid: ' + str(len(invalid)) + '/' + str(len(all_cv)))
     # TODO relate all researchers to all fields
