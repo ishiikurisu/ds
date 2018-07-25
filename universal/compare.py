097ea59df1d59533e17f2d3b5a8ba5dc488dbc81
Cristiano Alves da Silva Júnior <ishiikurisu@yahoo.com>
2018-07-25 16:44:21 -0300

    saved area table including classes

--- universal/compare.py
+++ universal/compare.py
@@ -12,6 +12,17 @@ def include_class_by_cv(areas_table, class_by_cv):
         areas_table['class'][cvs.index(cv)] = class_by_cv[cv]
     return areas_table
 
+def save_table(table, where):
+    limit = max([len(table[key]) for key in table])
+    columns = list(table.keys())
+    with open(where, 'w') as fp:
+        fp.write('%s\n' % '\t'.join(columns))
+        for i in range(limit):
+            stuff = []
+            for column in columns:
+                stuff.append(table[column][i])
+            fp.write('%s\n' % ('\t'.join(stuff)))
+
 if __name__ == '__main__':
     config = util.load_config(sys.argv[1])
     classes = config['classes']
@@ -19,5 +30,4 @@ if __name__ == '__main__':
     areas_table = util.load_csv(config['pwd'] + 'cv.csv')
     class_by_cv = parse_class_by_cv(classes_table)
     areas_table = include_class_by_cv(areas_table, class_by_cv)
-    print(areas_table)
-    # TODO save new areas table
+    save_table(areas_table, config['pwd'] + 'cv-class.csv')
