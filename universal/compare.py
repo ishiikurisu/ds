e70ada5be231d8d9457577e4bb80c02ee63a82ba
Cristiano Alves da Silva Júnior <ishiikurisu@yahoo.com>
2018-07-25 16:34:03 -0300

    parsing classes from classes table

--- universal/compare.py
+++ universal/compare.py
@@ -1,9 +1,14 @@
 import sys
 import util
 
+def parse_class_by_cv(table):
+    return { it[0]: it[1] for it in zip([it + '.xml' for it in table['Id']], table['modularity_class']) }
+
 if __name__ == '__main__':
     config = util.load_config(sys.argv[1])
     classes = config['classes']
-    table = util.load_csv(config['pwd'] + config['classes file'])
-    # TODO Group people by classes
-    # TODO Indicate most common knowledge area by class
+    classes_table = util.load_csv(config['pwd'] + config['classes file'])
+    areas_table = util.load_csv(config['pwd'] + 'cv.csv')
+    class_by_cv = parse_class_by_cv(classes_table)
+    # TODO include class column on area table
+    # TODO save new areas table
